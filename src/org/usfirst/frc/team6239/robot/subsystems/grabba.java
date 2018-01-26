966c23acebf8ea6736d01393372353c4ba72eaf0
kane <kane@kane-GS63VR-7RF>
2018-01-25 21:25:22 -0500

    grabber sub

--- src/org/usfirst/frc/team6239/robot/subsystems/grabba.java
+++ src/org/usfirst/frc/team6239/robot/subsystems/grabba.java
@@ -1,24 +1,28 @@
 package org.usfirst.frc.team6239.robot.subsystems;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.command.Subsystem;
+
+import org.usfirst.frc.team6239.robot.Robot;
 import org.usfirst.frc.team6239.robot.RobotMap;
 
 public class grabba extends Subsystem {
 
-	Spark mauddah = RobotMap.;
 
     public void initDefaultCommand() {
     }
 
     public void open() {
-    	motor.set(1);
+    	Robot.robotmap.grabberLeft.set(1);
+    	Robot.robotmap.grabberRight.set(-1);
     }
 
     public void close() {
-    	motor.set(-1);
+    	Robot.robotmap.grabberLeft.set(-1);
+    	Robot.robotmap.grabberRight.set(1);
     }
 
     public void stop() {
-    	motor.set(0);
+    	Robot.robotmap.grabberLeft.set(0);
+    	Robot.robotmap.grabberRight.set(0);
     }
 }
