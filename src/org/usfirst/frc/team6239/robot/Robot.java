074a0000cf4605664afae4e832f6ead8931e7126
TheMadGrammer <blueberryman2112@gmail.com>
2018-03-20 23:14:39 -0500

    NetworkTables

--- src/org/usfirst/frc/team6239/robot/Robot.java
+++ src/org/usfirst/frc/team6239/robot/Robot.java
@@ -8,6 +8,9 @@ import org.usfirst.frc.team6239.robot.subsystems.grabba;
 import org.usfirst.frc.team6239.robot.subsystems.liftsubsystem;
 import org.usfirst.frc.team6239.robot.swervedrive.SwerveDrive;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.command.Scheduler;
 
@@ -22,13 +25,22 @@ public class Robot extends IterativeRobot {
 
 
 
-    public  static RobotMap robotmap;
-    public static  grabba grabber;
-    public  static OI oi;
-    public static  DriveSubsystem DRIVE_SUB;
-    public  static liftsubsystem liftsub;
-    public  static DriveButtons drivebuttons;
+    public static RobotMap robotmap;
+    public static grabba grabber;
+    public static OI oi;
+    public static DriveSubsystem DRIVE_SUB;
+    public static liftsubsystem liftsub;
+    public static DriveButtons drivebuttons;
 	public static SwerveDrive driveTrain;
+	public static NetworkTable table;
+	public static NetworkTableInstance inst;
+	
+	public static NetworkTableEntry frontRightEnc;
+	public static NetworkTableEntry backLeftEnc;
+	public static NetworkTableEntry frontLeftEnc;
+	public static NetworkTableEntry backRightEnc;
+	public static NetworkTableEntry navXYaw;
+	
 	@Override
 	public void robotInit() {
 
@@ -39,7 +51,15 @@ public class Robot extends IterativeRobot {
 		drivebuttons = new DriveButtons();
 		oi = new OI();
 		driveTrain = new SwerveDrive(robotmap.speedController,robotmap.rotationalController,robotmap.encController);
-
+		inst = NetworkTableInstance.getDefault();
+		table = inst.getTable("NET_TABLE");
+		
+		frontRightEnc = table.getEntry("frontRight");
+		backRightEnc = table.getEntry("backRight");
+		frontLeftEnc = table.getEntry("frontLeft");
+		backLeftEnc = table.getEntry("backLeft");
+		navXYaw = table.getEntry("Yaw");
+		
 		
 	}
 	
@@ -83,12 +103,6 @@ public class Robot extends IterativeRobot {
 
 	@Override
 	public void autonomousPeriodic() {
-
-		
-		
-		
-		
-		
 		
 		Scheduler.getInstance().run();
 		
@@ -99,7 +113,11 @@ public class Robot extends IterativeRobot {
 	public void teleopPeriodic() {
 		Scheduler.getInstance().run();
 
-		
+		frontRightEnc.setDouble(robotmap.frontRightRotEnc.get());
+		backLeftEnc.setDouble(robotmap.backLeftRotControl.get());
+		frontLeftEnc.setDouble(robotmap.frontLeftRotControl.get());
+		backRightEnc.setDouble(robotmap.backRightRotEnc.get());
+		navXYaw.setDouble(robotmap.navX.getAngle());
 		
 	}
 }
